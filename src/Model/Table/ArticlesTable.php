c672a2549265432b545f9669207dfc96b95a7aea
Saarenpaa <92503594+Saarenpaa@users.noreply.github.com>
2022-05-01 17:41:29 +0300

    2

--- src/Model/Table/ArticlesTable.php
+++ src/Model/Table/ArticlesTable.php
@@ -1,12 +1,22 @@
 <?php
 
-use \Cake\ORM\Table;
+namespace App\Model\Table;
 
+use Cake\ORM\Table;
+use Cake\Utility\Text;
+use CAke\Event\EventInterface;
 class ArticlesTable extends Table
 {
     public function initialize(array $config): void
     {
-        $this-> addBehavior('Timestamp');
+        $this->addBehavior('Timestamp');
+    }
+    public function beforeSave(EventInterface $event, $entity, $options){
+        if ($entity->isNew() && !$entity->slug){
+            $sluggedTitle = Text::slug($entity->title);
+
+            $entity->slug = substr($sluggedTitle, 0, 19);
+        }
     }
 }
 ?>
